7e5ea9e00a687f997a9c9e757325c273c7e95aba
Youjaeyoung <dbwodud5942@naver.com>
2024-07-23 14:13:39 +0900

    cc

--- opencv/first/helloworld.cpp
+++ opencv/first/helloworld.cpp
@@ -5,5 +5,9 @@
 int main()
 {
     std::cout << "hello, world" << std::endl;
+    cv::Mat img;
+    img = cv::imread("lenna.bmp");
+    cv::imshow("image", img);
+    cv::waitKey(0);
     return 0;
 }
